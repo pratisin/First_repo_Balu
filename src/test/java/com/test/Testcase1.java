1a8cad8f00277f91a4c603a1f5e4ab596d5017ca
balveer.murki <balveer.murki@servicenow.com>
2020-07-08 17:58:20 +0530

    updated for json api test

--- src/test/java/com/test/Testcase1.java
+++ src/test/java/com/test/Testcase1.java
@@ -1,5 +1,15 @@
 package com.test;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
@@ -7,8 +17,22 @@ public class Testcase1 {
 
 	@Test
 	@Category(com.test.Testcase1.class)
-	public void testmethod1() {
-//		fail("Not yet implemented");
+	public void testmethod1() throws JSONException, IOException {
+		
+		
+		String payload = getPayload("gitLabjob.json");
+		JSONObject json = new JSONObject(payload);
+		System.out.println(json.get("build_id"));
+		
+		
+		System.out.println("after update");
+		json.put("build_id", 101);
+		System.out.println(json.get("build_id"));
+		
+		
+		
+		
+		
 	}
 	
 	
@@ -17,5 +41,26 @@ public class Testcase1 {
 	public void testmethod2() {
 //		fail("Not yet implemented");
 	}
+	
+	public static String getPayload(final String fileNm) throws IOException, JSONException {
+
+		final File file = new File("src/test/resources/" + fileNm);
+
+		final Path path = file.toPath();
+		System.out.println("-------------path for json file :" + file.getAbsolutePath());
+
+		final StringBuilder sb = new StringBuilder();
+		final List<String> content = Files.readAllLines(path, Charset.defaultCharset());
+
+		for (final String string : content) {
+
+			sb.append(string);
+		}
+
+		final String jsonstring = sb.toString();
+
+		return jsonstring;
+	}
+
 
 }
