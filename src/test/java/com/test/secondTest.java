9cee60eb9430a7a7d3cd2c0110a959e4199d22c7
balveer.murki <balveer.murki@servicenow.com>
2020-10-05 16:39:36 +0530

    updated to see

--- src/test/java/com/test/secondTest.java
+++ src/test/java/com/test/secondTest.java
@@ -1,15 +1,19 @@
 package com.test;
 
+import static org.testng.Assert.assertFalse;
+
+//import static org.junit.Assert.assertFalse;
+
 import org.testng.Assert;
 import org.testng.SkipException;
 import org.testng.annotations.Test;
 
 public class secondTest {
 
-	@Test
-	public void fail1() {
-		throw new NullPointerException("Will fail");
-	}
+//	@Test
+//	public void fail1() {
+//		throw new NullPointerException("Will fail");
+//	}
 
 //	@Test
 //	public void fail2() throws IOException {
@@ -21,20 +25,20 @@ public class secondTest {
 //		throw new RuntimeException();
 //	}
 
-	@Test	
+	@Test
 	public void test2() {
-		
-		for ( int i=0;i < 2000; i++)
+
+		for (int i = 0; i < 2000; i++)
 			System.out.println("testing jenkins....");
-			
+
 		Assert.assertTrue(true, "test is passed");
 	}
 
-	
-	/*
-	 * @Test public void test5() { assertFalse(true, "test is failed"); }
-	 */
-	
+	@Test
+	public void test5() {
+		assertFalse(true, "test is failed");
+	}
+
 	@Test
 	public void testskipped() {
 		throw new SkipException("skipped");
